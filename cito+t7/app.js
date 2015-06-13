c8fd36f0a773c1ba74db01e64e3bb5ceebb5ee5e
Dominic Gannaway <dominic@101010digital.com>
2015-06-13 23:26:18 +0100

    Added the Cito framework with the t7 template library (along with the precompiled t7 template)

---
+++ cito+t7/app.js
@@ -0,0 +1,58 @@
+var rootNode = null;
+
+function queries(query) {
+  return t7`
+    <td class="${ 'Query ' + query.elapsedClassName }">
+      <span class="foo">${ query.formatElapsed }</span>
+      <div class="popover left">
+        <div class="popover-content">${ query.query }</div>
+        <div class="arrow"></div>
+      </div>
+    </td>
+  `
+};
+
+function database(db) {
+  return t7`
+    <tr key="${ db.name }">
+
+      <td class="dbname">
+        ${ db.name }
+      </td>
+
+      <td class="query-count">
+        <span class="${ db.lastSample.countClassName }">
+          ${ db.lastSample.queries.length }
+        </span>
+      </td>
+
+      ${ db.lastSample.topFiveQueries.map( queries) }
+
+    </tr>
+  `;
+};
+
+function loadSamples() {
+  var dbs = ENV.generateData().toArray();
+
+  var table = t7`
+    <table class="table table-striped latest-data">
+      <tbody>
+        ${ dbs.map( database )}
+      </tbody>
+    </table>
+  `;
+
+  if(rootNode === null) {
+    rootNode = cito.vdom.append(document.getElementById("app"), table);
+  } else {
+    cito.vdom.update(rootNode, table);
+  }
+
+  Monitoring.renderRate.ping();
+  setTimeout(loadSamples, ENV.timeout);
+};
+
+$(function() {
+  loadSamples();
+});
