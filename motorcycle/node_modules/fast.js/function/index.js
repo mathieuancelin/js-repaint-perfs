aaa83aa9ca451515a0fe84667882e9ea0dc48d32
TylorS <tlsteinberger167@gmail.com>
2015-12-23 14:46:13 -0500

    fix build error

--- motorcycle/node_modules/fast.js/function/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.apply = require('./apply');
-exports.bind = require('./bind');
-exports.partial = require('./partial');
-exports.partialConstructor = require('./partialConstructor');
-exports.try = require('./try');
