9adb847df70b893ded0edb8297c2edfaca1a3c94
TylorS <tlsteinberger167@gmail.com>
2016-02-02 17:02:49 -0500

    write motorcycle app in ~es5

--- motorcycle/src/app.js
+++ motorcycle/src/app.js
@@ -1,47 +1,55 @@
 import most from 'most'
 import {run} from '@motorcycle/core'
 import {makeDOMDriver, h} from '@motorcycle/dom'
-import {map} from 'fast.js/array'
 
-const dbMap = q =>  h(`td.${q.elapsedClassName}`, [
-  h('span.foo', [q.formatElapsed]),
-  h('div.popover.left', [
-    h('div.popover-content', [
-      q.query
-    ]),
-    h('div.arrow')
+function dbMap(q) {
+  return h('td', {className: q.elapsedClassName}, [
+    h('span.foo', [q.formatElapsed]),
+    h('div.popover.left', [
+      h('div.popover-content', [
+        q.query
+      ]),
+      h('div.arrow')
+    ])
   ])
-])
+}
 
-const databasesMap = db =>  h('tr', [
-  h('td.dbname', [db.dbname]),
-  h('td.query-count', [
-    h(`span.${db.lastSample.countClassName}`, [
-      db.lastSample.nbQueries
-    ])
-  ]),
-  ...map(db.lastSample.topFiveQueries, dbMap)
-])
+function databasesMap(db) {
+  return h('tr', [
+    h('td.dbname', [db.dbname]),
+    h('td.query-count', [
+      h('span', {className: db.lastSample.countClassName}, [
+        db.lastSample.nbQueries
+      ])
+    ]),
+  ].concat(db.lastSample.topFiveQueries.map(dbMap)))
+}
 
-const main = sources => ({
-  DOM: sources.databases.map(databases => h('div', {static: true}, [
-    h('table.table.table-striped.latest-data', [
-      h('tbody', map(databases, databasesMap))
-    ])
-  ]))
-})
-
-const DBMONDriver = () => most.create(add => {
-  const load = () => {
-    add(ENV.generateData().toArray())
-    Monitoring.renderRate.ping()
-    setTimeout(load, ENV.timeout)
+function main(sources) {
+  return {
+    DOM: sources.databases.map(function(databases) {
+      return h ('div', {static: true}, [
+        h('table.table.table-striped.latest-data', {}, [
+          h('tbody', databases.map(databasesMap))
+        ])
+      ])
+    })
   }
-  load()
-})
+}
 
+
+function DBMONDriver(){
+  return most.create(function(add){
+    function load() {
+      add(ENV.generateData().toArray())
+      Monitoring.renderRate.ping()
+      setTimeout(load, ENV.timeout)
+    }
+    load()
+  })
+}
 
 run(main, {
-  DOM: makeDOMDriver('#app-container', [], true),
+  DOM: makeDOMDriver('#app-container'),
   databases: DBMONDriver
 })
