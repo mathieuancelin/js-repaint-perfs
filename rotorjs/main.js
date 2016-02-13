174adce6652d9b1a9b0b73e75004c82ad0564475
Kurakin Alexander <kuraga333@mail.ru>
2016-02-13 22:23:29 +0300

    Update RotorJS to 0.5.1

--- rotorjs/main.js
+++ rotorjs/main.js
@@ -1,3 +1,4 @@
+/** @jsx spreadH */
 'use strict';
 
 /* RotorJS Implementation
@@ -5,62 +6,90 @@
    For RotorJS, see https://github.com/kuraga/rotorjs
 */
 
+var _temporalUndefined = {};
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+var middleware = _temporalUndefined;
+
+var DbmonApplication = _temporalUndefined;
+var DbmonComponent = _temporalUndefined;
+
+var application = _temporalUndefined,
+    rootElement = _temporalUndefined;
+
+function _temporalAssertDefined(val, name, undef) { if (val === undef) { throw new ReferenceError(name + ' is not defined - temporal dead zone'); } return true; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var DbmonApplication = (function (_rotorjs$Application) {
-  _inherits(DbmonApplication, _rotorjs$Application);
+middleware = {
+  Cursor: rotorjsMiddlewares.Cursor_FreezerJs,
+  Loop: rotorjsMiddlewares.Loop_VirtualDom,
+  Trie: rotorjsMiddlewares.Trie_RouteTrie
+};
 
-  function DbmonApplication(rootElement) {
-    _classCallCheck(this, DbmonApplication);
+var _rotorjs$getRotorJsClasses = rotorjs.getRotorJsClasses(_temporalAssertDefined(middleware, 'middleware', _temporalUndefined) && middleware);
 
-    _get(Object.getPrototypeOf(DbmonApplication.prototype), 'constructor', this).call(this, rootElement);
+var Application = _rotorjs$getRotorJsClasses.Application;
+var Component = _rotorjs$getRotorJsClasses.Component;
+var RouterComponent = _rotorjs$getRotorJsClasses.RouterComponent;
+
+DbmonApplication = (function (_Application) {
+  _inherits(_temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication, _Application);
+
+  function DbmonApplication() {
+    _classCallCheck(this, _temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication);
+
+    _get(Object.getPrototypeOf((_temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication).prototype), 'constructor', this).call(this);
   }
 
-  _createClass(DbmonApplication, [{
+  _createClass(_temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication, [{
     key: 'start',
     value: function start() {
-      var dbmon = new DbmonComponent(this, null, 'dbmon');
-      _get(Object.getPrototypeOf(DbmonApplication.prototype), 'start', this).call(this, dbmon, 'dbmon');
+      var dbmon = _temporalUndefined;
+
+      dbmon = new (_temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent)(this, null, 'dbmon');
+      _get(Object.getPrototypeOf((_temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication).prototype), 'start', this).call(this, _temporalAssertDefined(dbmon, 'dbmon', _temporalUndefined) && dbmon);
     }
   }, {
     key: 'stop',
     value: function stop() {
-      _get(Object.getPrototypeOf(DbmonApplication.prototype), 'stop', this).call(this);
+      _get(Object.getPrototypeOf((_temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication).prototype), 'stop', this).call(this);
     }
   }]);
 
-  return DbmonApplication;
-})(rotorjs.Application);
+  return _temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication;
+})(_temporalAssertDefined(Application, 'Application', _temporalUndefined) && Application);
 
-var DbmonComponent = (function (_rotorjs$Component) {
-  _inherits(DbmonComponent, _rotorjs$Component);
+DbmonComponent = (function (_Component) {
+  _inherits(_temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent, _Component);
 
   function DbmonComponent(application, parent, name) {
-    _classCallCheck(this, DbmonComponent);
+    var initialState = _temporalUndefined;
 
-    var initialState = {
+    _classCallCheck(this, _temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent);
+
+    initialState = {
       databases: []
     };
-    _get(Object.getPrototypeOf(DbmonComponent.prototype), 'constructor', this).call(this, application, parent, name, initialState);
+    _get(Object.getPrototypeOf((_temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent).prototype), 'constructor', this).call(this, application, parent, name, _temporalAssertDefined(initialState, 'initialState', _temporalUndefined) && initialState);
   }
 
-  _createClass(DbmonComponent, [{
+  _createClass(_temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent, [{
     key: 'activate',
     value: function activate() {
-      _get(Object.getPrototypeOf(DbmonComponent.prototype), 'activate', this).call(this);
+      _get(Object.getPrototypeOf((_temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent).prototype), 'activate', this).call(this);
 
       this.loadSamples();
     }
   }, {
     key: 'deactivate',
     value: function deactivate() {
-      _get(Object.getPrototypeOf(DbmonComponent.prototype), 'deactivate', this).call(this);
+      _get(Object.getPrototypeOf((_temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent).prototype), 'deactivate', this).call(this);
     }
   }, {
     key: 'loadSamples',
@@ -124,20 +153,29 @@ var DbmonComponent = (function (_rotorjs$Component) {
     }
   }]);
 
-  return DbmonComponent;
-})(rotorjs.Component);
+  return _temporalAssertDefined(DbmonComponent, 'DbmonComponent', _temporalUndefined) && DbmonComponent;
+})(_temporalAssertDefined(Component, 'Component', _temporalUndefined) && Component);
 
-var application = undefined,
-    rootElement = undefined;
-
+application = undefined;
+rootElement = undefined;
 window.onload = function () {
+  _temporalAssertDefined(_temporalAssertDefined(application, 'application', _temporalUndefined) && application, 'application', _temporalUndefined);
+
+  application = new (_temporalAssertDefined(_temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication, 'DbmonApplication', _temporalUndefined) && _temporalAssertDefined(DbmonApplication, 'DbmonApplication', _temporalUndefined) && DbmonApplication)();
+
+  (_temporalAssertDefined(application, 'application', _temporalUndefined) && application).start();
+
+  _temporalAssertDefined(_temporalAssertDefined(rootElement, 'rootElement', _temporalUndefined) && rootElement, 'rootElement', _temporalUndefined);
+
   rootElement = document.getElementById('dbmon');
-  application = new DbmonApplication(rootElement);
-  application.start();
+
+  (_temporalAssertDefined(rootElement, 'rootElement', _temporalUndefined) && rootElement).appendChild((_temporalAssertDefined(application, 'application', _temporalUndefined) && application).target);
 };
 
 window.onunload = function () {
-  application.stop();
+  (_temporalAssertDefined(application, 'application', _temporalUndefined) && application).stop();
+
+  (_temporalAssertDefined(rootElement, 'rootElement', _temporalUndefined) && rootElement).removeChild((_temporalAssertDefined(application, 'application', _temporalUndefined) && application).target);
 };
 
 // Dirty fix, see https://github.com/Matt-Esch/virtual-dom/pull/297/files
