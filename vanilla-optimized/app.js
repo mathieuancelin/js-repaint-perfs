ea7228cc657c61321d1eef4fadf728f0b74b9083
Isiah Meadows <impinball@gmail.com>
2017-03-06 13:38:38 -0500

    Remove HTML generation
    Perf boost.

--- vanilla-optimized/app.js
+++ vanilla-optimized/app.js
@@ -1,65 +1,114 @@
-var lastDatabases = [];
-
-var render = function () {
-
-    function renderTable() {
-        var html = '<table class="table table-striped latest-data"><tbody id="table">';
-        html += renderRows();
-        html +='</tbody></table>';
-
-        return html;
+;(function () {
+    var lastDatabases = ENV.generateData().toArray();
+    var nodes = [];
+    var app = document.getElementById('app');
+    var table = document.createElement('table');
+    var tbody = document.createElement('tbody');
+    table.className = 'table table-striped latest-data';
+    tbody.id = 'table';
+    while (app.firstChild) app.removeChild(app.firstChild);
+    app.appendChild(table);
+    table.appendChild(tbody);
+    for (var i = 0; i < lastDatabases.length; i++) {
+        renderRow(lastDatabases[i]);
     }
-
-    function renderRows() {
-        var html = '';
-        for (var index = 0; index < databases.length; index++) {
-            var db = databases[index];
-            html += '<tr>' + renderRow(db) + '</tr>';
-        }
-        return html;
-    }
-
+    
     function renderRow(db) {
-        var html = '<td class="dbname">' + db.dbname + '</td>';
-        html += '<td class="query-count">';
-        html += '<span class="' + db.lastSample.countClassName + '">' + db.lastSample.nbQueries + '</span></td>';
+        var row = document.createElement('tr');
+        var dbname = document.createElement('td');
+        var lastSample = document.createElement('td');
+        var lastSampleSpan = document.createElement('span');
+        var top5 = [];
 
-        for (var indexQ = 0; indexQ < db.lastSample.topFiveQueries.length; indexQ++) {
-            var q = db.lastSample.topFiveQueries[indexQ];
-            html += '<td class="' + q.elapsedClassName + '">';
-            html +=     q.formatElapsed || '';
-            html +=     '<div class="popover left">';
-            html +=         '<div class="popover-content">';
-            html +=             q.query || '';
-            html +=         '</div>';
-            html +=         '<div class="arrow"></div>';
-            html +=     '</div>';
-            html += '</td>';
+        dbname.className = 'dbname';
+        lastSample.className = 'query-count';
+        lastSampleSpan.className = db.lastSample.countClassName;
+
+        tbody.appendChild(row);
+        row.appendChild(dbname);
+        row.appendChild(lastSample);
+        lastSample.appendChild(lastSampleSpan);
+        dbname.textContent = db.dbname;
+        lastSampleSpan.textContent = db.lastSample.nbQueries;
+        
+        for (var i = 0; i < db.lastSample.topFiveQueries.length; i++) {
+            var q = db.lastSample.topFiveQueries[i];
+            var query = document.createElement('td');
+            var elapsed = document.createTextNode(q.formatElapsed || '');
+            var popover = document.createElement('div');
+            var popoverContent = document.createElement('div');
+            var arrow = document.createElement('div');
+
+            query.className = q.elapsedClassName;
+            popover.className = 'popover left';
+            popoverContent.className = 'popover-content';
+            arrow.className = 'arrow';
+
+            row.appendChild(query);
+            query.appendChild(elapsed);
+            query.appendChild(popover);
+            popover.appendChild(popoverContent);
+            popoverContent.textContent = q.query || '';
+            popover.appendChild(arrow);
+            top5.push({
+                query: query,
+                elapsed: elapsed,
+                popover: popoverContent
+            });
         }
-        return html;
+        
+        nodes.push({
+            dbname: dbname,
+            lastSample: lastSampleSpan,
+            top5: top5
+        });
     }
+    
+    
+    function patchRow(node, last, current) {
+        if (last.dbname !== current.dbname) {
+            node.dbname.textContext = current.dbname;
+        }
 
-    var databases = ENV.generateData().toArray();
+        if (last.lastSample.countClassName !== current.lastSample.countClassName) {
+            node.lastSample.className = current.lastSample.countClassName;
+        }
 
-    if (lastDatabases.length !== databases.length) {
-        document.getElementById("app").innerHTML = renderTable();
-    } else {
-        var table = document.getElementById("table");
+        if (last.lastSample.nbQueries !== current.lastSample.nbQueries) {
+            node.lastSample.textContent = current.lastSample.nbQueries
+        }
 
-        for (var index = 0; index < databases.length; index++) {
-            var db = databases[index];
-            var lastDb = lastDatabases[index];
-            if (renderRow(db) !== renderRow(lastDb)) {
-                table.children.item(index).innerHTML = renderRow(db);
+        for (var i = 0; i < node.top5.length; i++) {
+            var child = node.top5[i];
+            var lastQ = last.lastSample.topFiveQueries[i];
+            var currentQ = current.lastSample.topFiveQueries[i];
+
+            if (lastQ.elapsedClassName !== currentQ.elapsedClassName) {
+                child.query.className = currentQ.elapsedClassName;
+            }
+
+            if (lastQ.formatElapsed !== currentQ.formatElapsed) {
+                child.elapsed.textContent = currentQ.formatElapsed;
+            }
+
+            if (lastQ.query !== currentQ.query) {
+                child.popoverContent.textContent = currentQ.query;
             }
         }
     }
 
+    function render() {
+        var databases = ENV.generateData().toArray();
 
-    lastDatabases = databases;
+        for (var i = 0; i < databases.length; i++) {
+            patchRow(nodes[i], lastDatabases[i], databases[i]);
+        }
 
+        lastDatabases = databases;
+        Monitoring.renderRate.ping();
+        setTimeout(render, ENV.timeout);
+    }
+    
     Monitoring.renderRate.ping();
     setTimeout(render, ENV.timeout);
-};
-
-render();
+})();
